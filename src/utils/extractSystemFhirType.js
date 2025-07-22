c30a7c64990d564ac3a6c72700446830b177c76f
mechanik-daniel <daniel@outburn.co.il>
2025-07-22 14:16:26 +0300

    refactor: cleanup uncovered code in extractSystemFhirType

--- src/utils/extractSystemFhirType.js
+++ src/utils/extractSystemFhirType.js
@@ -1,11 +1,7 @@
 // when an element is a system primitive, it may still have a fhir type defined in an extension.
 // this type code should be used for extracting fixed[x] and pattern[x] keys, and for regex validation
 const extractSystemFhirType = (elementDefinitionType) => {
-  const extension = elementDefinitionType.extension?.find(e => e.url === 'http://hl7.org/fhir/StructureDefinition/structuredefinition-fhir-type');
-  if (extension && extension.valueUrl) {
-    return extension.valueUrl;
-  }
-  return '';
+  return elementDefinitionType.extension?.find(e => e.url === 'http://hl7.org/fhir/StructureDefinition/structuredefinition-fhir-type')?.valueUrl;
 };
 
 export default extractSystemFhirType;
